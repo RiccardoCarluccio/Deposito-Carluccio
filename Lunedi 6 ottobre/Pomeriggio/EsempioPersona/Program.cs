c2861cf3ffb77ad6a0ba4a72381367776686bb4a
Riccardo Carluccio <riccardo.carluccio.93@gmail.com>
2025-10-06 16:58:53 +0200

    add person choice

--- Lunedi 6 ottobre/Pomeriggio/EsempioPersona/Program.cs
+++ Lunedi 6 ottobre/Pomeriggio/EsempioPersona/Program.cs
@@ -1,12 +1,14 @@
 ﻿Persona persona_1 = new Persona("Mario", "Rossi", 1990);
 Persona persona_2 = new Persona("Giuseppe", "Verdi", 1980);
 
-Console.WriteLine($"{persona_1.Nome} {persona_1.Cognome} è nato nell'anno {persona_1.AnnoNascita}");
-Console.WriteLine($"{persona_2.Nome} {persona_2.Cognome} è nato nell'anno {persona_2.AnnoNascita}");
+Console.WriteLine($"{persona_1.Nome} {persona_1.Cognome} è nato nell'anno: {persona_1.AnnoNascita}");
+Console.WriteLine($"{persona_2.Nome} {persona_2.Cognome} è nato nell'anno: {persona_2.AnnoNascita}");
 
 Persona[] listaPersone = [persona_1, persona_2 ];
 
 Console.WriteLine($"Ci sono {listaPersone.Length} persone nel database. Quale vuoi visualizzare?");
 int input = int.Parse(Console.ReadLine()!);
+input--;
 
 Console.WriteLine($"Hai deciso di visualizzare: {listaPersone[input].Nome}");
+Console.WriteLine($"{listaPersone[input].Nome} {listaPersone[input].Cognome} è nato nell'anno: {listaPersone[input].AnnoNascita}");
