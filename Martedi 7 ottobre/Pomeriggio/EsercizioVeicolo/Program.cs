cb467028e8136f6656effbd76f601b4386a16bc1
Riccardo Carluccio <riccardo.carluccio.93@gmail.com>
2025-10-07 16:32:43 +0200

    add EsercizioVeicolo

---
+++ Martedi 7 ottobre/Pomeriggio/EsercizioVeicolo/Program.cs
@@ -0,0 +1,57 @@
+﻿List<Veicolo> veicoli = new();
+
+int input;
+do
+{
+    Console.WriteLine("Seleziona l'operazione da eseguire:\n" +
+        "1. Inserire nuovo veicolo\n" +
+        "2. Visualizzare tutti i veicoli presenti\n" +
+        "3. Uscire dal programma");
+
+    input = int.Parse(Console.ReadLine()!);
+
+    switch (input)
+    {
+        case 1:
+            Console.WriteLine("Che tipo di veicolo vuoi aggiungere?\n" +
+                "1. Auto\n" +
+                "2. Moto\n" +
+                "3. Torna indietro");
+            int tipo = int.Parse(Console.ReadLine()!);
+
+            if (tipo == 3)
+                break;
+
+            Veicolo nuovoVeicolo = new();
+            switch (tipo)
+            {
+                case 1:
+                    nuovoVeicolo = new Auto();
+                    break;
+                case 2:
+                    nuovoVeicolo = new Moto();
+                    break;
+                default:
+                    Console.WriteLine("Scelta non valida\n");
+                    break;
+            }
+            nuovoVeicolo.AggiungiVeicolo();
+            veicoli.Add(nuovoVeicolo);
+            break;
+
+        case 2:
+            if (veicoli.Count == 0)
+                Console.WriteLine("Non ci sono veicoli presenti\n");
+            else
+            {
+                foreach (var veicolo in veicoli)
+                    veicolo.StampaInfo();
+                Console.WriteLine("\n\n");
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+while (input != 3);
---
+++ Martedi 7 ottobre/Pomeriggio/EsercizioVeicolo/Veicoli/Veicoli.cs
@@ -0,0 +1,54 @@
+﻿public class Veicolo
+{
+    public string Marca;
+    public string Modello;
+
+    public virtual void StampaInfo()
+    {
+        Console.WriteLine($"Marca: {Marca}, Modello: {Modello}");
+    }
+
+    public virtual void AggiungiVeicolo()
+    {
+        Console.Write("\nInserisci la marca: ");
+        Marca = Console.ReadLine()!;
+        Console.Write("\nInserisci il modello: ");
+        Modello = Console.ReadLine()!;
+    }
+}
+
+public class Auto : Veicolo
+{
+    public int NumeroPorte;
+
+    public override void StampaInfo()
+    {
+        base.StampaInfo();
+        Console.WriteLine($"Numero Porte: {NumeroPorte}");
+    }
+
+    public override void AggiungiVeicolo()
+    {
+        base.AggiungiVeicolo();
+        Console.Write("\nInserisci il numero di porte: ");
+        NumeroPorte = int.Parse(Console.ReadLine()!);
+    }
+}
+
+public class Moto : Veicolo
+{
+    public string TipoManubrio;
+
+    public override void StampaInfo()
+    {
+        base.StampaInfo();
+        Console.WriteLine($"Tipo Manubrio: {TipoManubrio}");
+    }
+
+    public override void AggiungiVeicolo()
+    {
+        base.AggiungiVeicolo();
+        Console.Write("\nInserisci il tipo di manubrio: ");
+        TipoManubrio = Console.ReadLine()!;
+    }
+}
