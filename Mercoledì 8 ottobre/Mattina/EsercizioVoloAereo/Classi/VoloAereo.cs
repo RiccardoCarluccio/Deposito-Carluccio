59a7049786cfac2ad852b1d7b93c13649abd2164
Riccardo Carluccio <riccardo.carluccio.93@gmail.com>
2025-10-08 11:07:55 +0200

    add EsercizioVoloAereo

---
+++ Mercoledì 8 ottobre/Mattina/EsercizioVoloAereo/Classi/VoloAereo.cs
@@ -0,0 +1,56 @@
+﻿namespace EsercizioVoloAereo.Classi;
+
+public class VoloAereo
+{
+    private int postiOccupati;
+    private const int MaxPosti = 150;
+    public int CodiceVolo { get; set; }
+    public int PostiOccupati
+    {
+        get { return postiOccupati; }
+    }
+    public int PostiLiberi
+    {
+        get { return MaxPosti - postiOccupati; }
+    }
+
+    public VoloAereo(int postiOccupati, int codiceVolo)
+    {
+        this.postiOccupati = postiOccupati;
+        this.CodiceVolo = codiceVolo;
+    }
+
+    public void EffettuaPrenotazioni(int numeroPosti)
+    {
+        if (numeroPosti <= PostiLiberi && numeroPosti + PostiOccupati <= 150)
+        {
+            postiOccupati += numeroPosti;
+            Console.WriteLine($"Sono stati prenotati {numeroPosti} posti");
+        }
+        else
+            Console.WriteLine($"I posti liberi sono solo {PostiLiberi}");
+
+        VisualizzaStato();
+    }
+
+    public void AnnullaPrenotazione(int numeroPosti)
+    {
+        if (numeroPosti <= PostiOccupati && numeroPosti + PostiLiberi <= 150)
+        {
+            postiOccupati -= numeroPosti;
+            Console.WriteLine($"è stata annullata la prenotazione di {numeroPosti} posti");
+        }
+        else
+            Console.WriteLine($"I posti occupati sono solo {PostiOccupati}");
+
+        VisualizzaStato();
+    }
+
+    public void VisualizzaStato()
+    {
+        Console.WriteLine(
+            $"Codice volo: {CodiceVolo}\n" +
+            $"Posti occupati: {PostiOccupati}\n" +
+            $"Posti liberi: {PostiLiberi}\n");
+    }
+}
---
+++ Mercoledì 8 ottobre/Mattina/EsercizioVoloAereo/Program.cs
@@ -0,0 +1,9 @@
+﻿using EsercizioVoloAereo.Classi;
+
+var voloAereo = new VoloAereo(50, 500);
+
+voloAereo.VisualizzaStato();
+
+voloAereo.EffettuaPrenotazioni(35);
+
+voloAereo.AnnullaPrenotazione(75);
