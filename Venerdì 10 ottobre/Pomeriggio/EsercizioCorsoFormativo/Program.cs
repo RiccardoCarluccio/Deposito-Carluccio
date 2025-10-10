c38f8daa8c91b977be6fe5775dca3ad9d159b830
Riccardo Carluccio <riccardo.carluccio.93@gmail.com>
2025-10-10 17:17:33 +0200

    add EsercizioCorsoFormativo

---
+++ Venerdì 10 ottobre/Pomeriggio/EsercizioCorsoFormativo/Program.cs
@@ -0,0 +1,16 @@
+﻿using EsercizioCorsoFormativo.Classi;
+using EsercizioCorsoFormativo.ClassiAstratte;
+
+List<Corso> corsi = new()
+{
+    new CorsoInPresenza("Infermiere", 900, "2B", 150),
+    new CorsoOnline("Sviluppatore Web", 700, "1509", "https://link.accesso.com")
+};
+
+var corsoInPresenza = corsi[0];
+var corsoOnline = corsi[1];
+
+corsoInPresenza.StampaDettagli();
+corsoInPresenza.StampaDettagli(corsoInPresenza);
+
+corsoOnline.ErogaCorso();
