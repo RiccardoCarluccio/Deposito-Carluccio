cde890859866fed32971c0c8118326f7b9e7282e
Riccardo Carluccio <riccardo.carluccio.93@gmail.com>
2025-10-14 11:56:54 +0200

    add second factory

--- Martedi 14 ottobre/Mattina/Es_IVeicolo/Program.cs
+++ Martedi 14 ottobre/Mattina/Es_IVeicolo/Program.cs
@@ -65,23 +65,105 @@ public abstract class VeicoloFactory
     }
 }
 
+public interface INave
+{
+    void Avvia();
+    void MostraTipo();
+}
+
+public class BarcaAVela : INave
+{
+    public void Avvia()
+    {
+        Console.WriteLine("Alza le vele");
+    }
+
+    public void MostraTipo()
+    {
+        Console.WriteLine("Tipo: barca a vela");
+    }
+}
+
+public class BarcaAMotore : INave
+{
+    public void Avvia()
+    {
+        Console.WriteLine("Accendi il motore");
+    }
+
+    public void MostraTipo()
+    {
+        Console.WriteLine("Tipo: barca a motore");
+    }
+}
+
+public abstract class NaveFactory
+{
+    public static INave CreaNave(string tipo)
+    {
+        switch (tipo.ToLower())
+        {
+            case "vela":
+                return new BarcaAVela();
+
+            case "motore":
+                return new BarcaAMotore();
+
+            default:
+                Console.WriteLine("Tipo non valido");
+                return null;
+        }
+    }
+}
+
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Seleziona il veicolo da creare");
-        string input = Console.ReadLine()!;
+        Console.WriteLine("Seleziona il mezzo da creare:\n" +
+            "1. Veicolo\n" +
+            "2. Nave\n");
 
-        IVeicolo veicolo = VeicoloFactory.CreaVeicolo(input);
+        int input = int.Parse(Console.ReadLine()!);
 
-        if (veicolo != null)
+        switch (input)
         {
-            veicolo.Avvia();
-            veicolo.MostraTipo();
-        }
-        else
-        {
-            Console.WriteLine("Veicolo non valido");
-        }
+            case 1:
+                Console.WriteLine("Seleziona il veicolo da creare:\n" +
+                    "Auto, Moto, Camion");
+
+                string sceltaVeicolo = Console.ReadLine()!;
+                IVeicolo veicolo = VeicoloFactory.CreaVeicolo(sceltaVeicolo);
+
+                if (veicolo != null)
+                {
+                    veicolo.Avvia();
+                    veicolo.MostraTipo();
+                }
+                else
+                {
+                    Console.WriteLine("Veicolo non valido");
+                }
+                break;
+
+            case 2:
+                Console.WriteLine("Seleziona la nave da creare:\n" +
+                    "Vela, Motore");
+
+                string sceltaNave = Console.ReadLine()!;
+                INave nave = NaveFactory.CreaNave(sceltaNave);
+
+                if (nave != null)
+                {
+                    nave.Avvia();
+                    nave.MostraTipo();
+                }
+                else
+                {
+                    Console.WriteLine("Nave non valida");
+                }
+                break;
+
+        }        
     }
 }
