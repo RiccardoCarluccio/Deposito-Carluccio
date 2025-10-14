760fc7b0c3efda08c677bdd41b168bc2bf495f43
Riccardo Carluccio <riccardo.carluccio.93@gmail.com>
2025-10-14 12:47:47 +0200

    add Es_Forme

---
+++ Martedi 14 ottobre/Mattina/Es_Forme/Program.cs
@@ -0,0 +1,60 @@
+﻿public interface IShape
+{
+    void Draw();
+}
+
+public class Circle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("You drew a circle");
+    }
+}
+
+public class Square : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("You drew a square");
+    }
+}
+
+public abstract class ShapeCreator
+{
+    public abstract IShape CreateShape(string type);
+}
+
+public class ConcreteShapeCreator : ShapeCreator
+{
+    public override IShape CreateShape(string type)
+    {
+        switch (type.ToLower())
+        {
+            case "circle":
+                return new Circle();
+
+            case "square":
+                return new Square();
+
+            default:
+                Console.WriteLine("Invalid type");
+                return null;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ShapeCreator creator = new ConcreteShapeCreator();
+        
+        Console.WriteLine("Select the shape to draw\n" +
+            "Circle, Square");
+        string input = Console.ReadLine()!;
+
+        IShape shape = creator.CreateShape(input);
+
+        shape.Draw();
+    }
+}
