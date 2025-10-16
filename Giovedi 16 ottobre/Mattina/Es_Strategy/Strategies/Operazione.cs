7b8d71096e9dfcda42f14a49b8bb9151069546bb
Riccardo Carluccio <riccardo.carluccio.93@gmail.com>
2025-10-16 11:26:48 +0200

    fix correct user choice

--- Giovedi 16 ottobre/Mattina/Es_Strategy/Strategies/Operazione.cs
+++ Giovedi 16 ottobre/Mattina/Es_Strategy/Strategies/Operazione.cs
@@ -9,6 +9,7 @@ public class SommaStrategiaConcreta : IStrategiaOperazione
 {
     public double Calcola(double a, double b)
     {
+        Console.WriteLine("Somma: ");
         return a + b;
     }
 }
@@ -17,6 +18,7 @@ public class SottrazioneStrategiaConcreta : IStrategiaOperazione
 {
     public double Calcola(double a, double b)
     {
+        Console.WriteLine("Sottrazione: ");
         return a - b;
     }
 }
@@ -25,6 +27,7 @@ public class MoltiplicazioneStrategiaConcreta : IStrategiaOperazione
 {
     public double Calcola(double a, double b)
     {
+        Console.WriteLine("Moltiplicazione: ");
         return a * b;
     }
 }
@@ -33,6 +36,7 @@ public class DivisioneStrategiaConcreta : IStrategiaOperazione
 {
     public double Calcola(double a, double b)
     {
+        Console.WriteLine("Divisione: ");
         if (b == 0)
             throw new NotImplementedException();
         return a / b;
@@ -69,27 +73,52 @@ class Program
         double num1;
         double num2;
 
+        int input;
+        
+        Console.WriteLine("Scegli l'operazione:\n" +
+            "1. Addizione\n" +
+            "2. Sottrazione\n" +
+            "3. Moltiplicazione\n" +
+            "4. Divisione\n" +
+            "0. Chiudi programma");
+        input = int.Parse(Console.ReadLine()!);
+
+        switch (input)
+        {
+            case 1:
+                calcolatrice.ImpostaStrategia(new SommaStrategiaConcreta());
+                    
+                break;
+
+            case 2:
+                calcolatrice.ImpostaStrategia(new SottrazioneStrategiaConcreta());
+                    
+                break;
+
+            case 3:
+                calcolatrice.ImpostaStrategia(new MoltiplicazioneStrategiaConcreta());
+                    
+                break;
+
+            case 4:
+                calcolatrice.ImpostaStrategia(new DivisioneStrategiaConcreta());
+                    
+                break;
+
+            case 0:
+                break;
+
+            default:
+                break;
+        }
+
         Console.WriteLine("Inserisci 2 numeri\n" +
             "Primo numero:");
         num1 = double.Parse(Console.ReadLine()!);
 
         Console.WriteLine("Secondo numero:");
         num2 = double.Parse(Console.ReadLine()!);
-
-        calcolatrice.ImpostaStrategia(new SommaStrategiaConcreta());
-        Console.WriteLine("Somma: ");
-        calcolatrice.EseguiOperazione(num1, num2);
-
-        calcolatrice.ImpostaStrategia(new SottrazioneStrategiaConcreta());
-        Console.WriteLine("Sottrazione: ");
-        calcolatrice.EseguiOperazione(num1, num2);
-
-        calcolatrice.ImpostaStrategia(new MoltiplicazioneStrategiaConcreta());
-        Console.WriteLine("Moltiplicazione: ");
-        calcolatrice.EseguiOperazione(num1, num2);
-
-        calcolatrice.ImpostaStrategia(new DivisioneStrategiaConcreta());
-        Console.WriteLine("Divisione: ");
+        
         calcolatrice.EseguiOperazione(num1, num2);
     }
 }
