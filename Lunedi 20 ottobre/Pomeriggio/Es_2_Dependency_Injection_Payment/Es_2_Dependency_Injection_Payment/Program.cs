1644ce8cc46e4d6a396b9b3d3dc31128f34fabce
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2025-10-20 16:53:10 +0200

    add Es_2_Dependency_Injection_Payment

---
+++ Lunedi 20 ottobre/Pomeriggio/Es_2_Dependency_Injection_Payment/Es_2_Dependency_Injection_Payment/Program.cs
@@ -0,0 +1,66 @@
+﻿public interface IPaymentGateway
+{
+    void Pay(double cost);
+}
+
+public class PaypalGateway : IPaymentGateway
+{
+    public void Pay(double cost)
+    {
+        Console.WriteLine($"Payment of {cost} done with PayPal");
+    }
+}
+
+public class StripeGateway : IPaymentGateway
+{
+    public void Pay(double cost)
+    {
+        Console.WriteLine($"Payment of {cost} done with Stripe");
+    }
+}
+
+public class PaymentProcess
+{
+    private readonly IPaymentGateway _gateway;
+
+    public PaymentProcess(IPaymentGateway gateway)
+    {
+        _gateway = gateway;
+    }
+
+    public void Pay(double cost)
+    {
+        _gateway.Pay(cost);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IPaymentGateway? gateway = null;
+
+        Console.WriteLine("Choose your preferred payment method:\n" +
+            "1. PayPal\n" +
+            "2. Stripe");
+        int input = int.Parse(Console.ReadLine()!);
+
+        switch (input)
+        {
+            case 1:
+                gateway = new PaypalGateway();
+                break;
+
+            case 2:
+                gateway = new StripeGateway();
+                break;
+
+            default:
+                Console.WriteLine("Invalid input");
+                break;
+        }
+        
+        var paymentProcess = new PaymentProcess(gateway);
+        paymentProcess.Pay(25);
+    }
+}
