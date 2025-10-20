35f6b57777995b540387f928bfb217ac093d4365
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2025-10-20 16:11:25 +0200

    add Es_1_Dependency_Injection

---
+++ Lunedi 20 ottobre/Pomeriggio/Es_1_Dependency_Injection/Es_1_Dependency_Injection/Program.cs
@@ -0,0 +1,37 @@
+﻿public interface IGreeter
+{
+    void Greet(string message);
+}
+
+public class ConsoleGreeter : IGreeter
+{
+    public void Greet(string message)
+    {
+        Console.WriteLine($"Benvenuto, {message}");
+    }
+}
+
+public class GreetingService
+{
+    private readonly IGreeter _greeter;
+
+    public GreetingService(IGreeter greeter)
+    {
+        _greeter = greeter;
+    }
+
+    public void GreetUser(string user)
+    {
+        _greeter.Greet($"{user}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IGreeter greeter = new ConsoleGreeter();
+        GreetingService greetingService = new GreetingService(greeter);
+        greetingService.GreetUser("Mario");
+    }
+}
