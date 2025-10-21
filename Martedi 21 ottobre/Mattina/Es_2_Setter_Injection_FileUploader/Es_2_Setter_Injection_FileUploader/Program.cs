2a17f29aab439412c0b8b12840b80aac5c9c8760
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2025-10-21 10:58:48 +0200

    add Es_2_Setter_Injection_FileUploader

---
+++ Martedi 21 ottobre/Mattina/Es_2_Setter_Injection_FileUploader/Es_2_Setter_Injection_FileUploader/Program.cs
@@ -0,0 +1,54 @@
+﻿public interface IStorageService
+{
+    void Upload(string data);
+}
+
+public class DiskStorageService : IStorageService
+{
+    public void Upload(string data)
+    {
+        Console.WriteLine($"{data} " +
+            $"salvato su disco");
+    }
+}
+
+public class InMemoryStorageService : IStorageService
+{
+    public void Upload(string data)
+    {
+        Console.WriteLine($"{data} " +
+            $"salvato InMemory");
+    }
+}
+
+public class FileUploader
+{
+    public IStorageService storageService;
+
+    public void UploadFile(string data)
+    {
+        if (storageService == null)
+        {
+            Console.WriteLine("Dispositivo di salvataggio non presente");
+            return;
+        }
+        storageService.Upload(data);
+        Console.WriteLine("Upload completato");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var fileUploader = new FileUploader();
+
+        fileUploader.storageService = new DiskStorageService();
+        fileUploader.UploadFile("Data");
+
+        Console.WriteLine("");
+
+        fileUploader.storageService = new InMemoryStorageService();
+        fileUploader.UploadFile("Data");
+    }
+}
