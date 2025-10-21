9b4649ecc167df816a482f193078831db67af437
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2025-10-21 16:37:01 +0200

    add Es_2_Enum_LivelloAccesso

---
+++ Martedi 21 ottobre/Pomeriggio/Es_2_Enum_LivelloAccesso/Es_2_Enum_LivelloAccesso/Program.cs
@@ -0,0 +1,37 @@
+﻿public enum LivelloAccesso
+{
+    Ospite,
+    Utente,
+    Amministratore
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Scegli il tipo di utente per verificarne i privilegi\n" +
+            "1. Ospite\n" +
+            "2. Utente\n" +
+            "3. Amministratore");
+        int input = int.Parse(Console.ReadLine()!);
+
+        switch (input)
+        {
+            case ((int)LivelloAccesso.Ospite + 1):
+                Console.WriteLine("L'ospite può solo visionare la home page");
+                break;
+
+            case ((int)LivelloAccesso.Utente + 1):
+                Console.WriteLine("L'utente può fare l'accesso all'area riservata");
+                break;
+
+            case ((int)LivelloAccesso.Amministratore + 1):
+                Console.WriteLine("L'amministratore può gestire le configurazioni degli utenti");
+                break;
+
+            default:
+                Console.WriteLine("Input non valido");
+                break;
+        }
+    }
+}
