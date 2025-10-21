d5eea1cbf8d2ef55db6877222f1a4a14e2944f55
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2025-10-21 10:45:54 +0200

    add Es_1_Setter_Injection_Printer

---
+++ Martedi 21 ottobre/Mattina/Es_1_Setter_Injection_Printer/Es_1_Setter_Injection_Printer/Program.cs
@@ -0,0 +1,38 @@
+﻿public interface ILogger
+{
+    void Log(string message);
+}
+
+public class Logger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"[LOG]: {message}");
+    }
+}
+
+public class Printer
+{
+    public ILogger? logger { get; set; }
+
+    public void Print()
+    {
+        if (logger == null )
+        {
+            Console.WriteLine("Logger non impostato");
+            return;
+        }
+        logger.Log("Stampa messaggio");
+        Console.WriteLine("Messaggio stampato con successo");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var printer = new Printer();
+        printer.logger = new Logger();
+        printer.Print();
+    }
+}
