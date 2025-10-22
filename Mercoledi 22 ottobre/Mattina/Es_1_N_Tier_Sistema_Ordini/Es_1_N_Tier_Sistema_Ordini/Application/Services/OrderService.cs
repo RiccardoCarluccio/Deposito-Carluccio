79785d9c965b5d442ae786b7f86a24e29ff4b920
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2025-10-22 15:06:05 +0200

    add ProductService (Application)

---
+++ Mercoledi 22 ottobre/Mattina/Es_1_N_Tier_Sistema_Ordini/Es_1_N_Tier_Sistema_Ordini/Application/Services/OrderService.cs
@@ -0,0 +1,6 @@
+﻿namespace Es_1_N_Tier_Sistema_Ordini.Application.Services;
+
+public class OrderService
+{
+
+}
---
+++ Mercoledi 22 ottobre/Mattina/Es_1_N_Tier_Sistema_Ordini/Es_1_N_Tier_Sistema_Ordini/Application/Services/ProductService.cs
@@ -0,0 +1,36 @@
+﻿using Es_1_N_Tier_Sistema_Ordini.Domain.Entities;
+using Es_1_N_Tier_Sistema_Ordini.Domain.Interfaces;
+
+namespace Es_1_N_Tier_Sistema_Ordini.Application.Services;
+
+public class ProductService
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductService(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public Product? GetProductByCode(string code)
+    {
+        return _productRepository.GetByCode(code);
+    }
+
+    public void AddProduct(string code, string name, double price)
+    {
+        var product = new Product
+        {
+            Code = code,
+            Name = name,
+            Price = price
+        };
+
+        _productRepository.Add(product);
+    }
+
+    public IEnumerable<Product> ListProducts()
+    {
+        return _productRepository.List();
+    }
+}
