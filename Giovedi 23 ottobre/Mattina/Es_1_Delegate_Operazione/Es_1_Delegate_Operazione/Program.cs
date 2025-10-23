d61854024ca421cea3679d728a2b28cfbf18966f
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2025-10-23 11:14:30 +0200

    add Es_1_Delegate_Operazione

---
+++ Giovedi 23 ottobre/Mattina/Es_1_Delegate_Operazione/Es_1_Delegate_Operazione/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+
+class Program
+{
+    delegate int Operazione(int a, int b);
+
+    static void Main()
+    {
+        int Somma(int a, int b)
+        {
+            return a + b;
+        }
+
+        int Moltiplicazione(int a, int b)
+        {
+            return a * b;
+        }
+
+        int EseguiOperazione(int a, int b, Operazione operazione)
+        {
+            return operazione(a, b);
+        }
+
+        Operazione op_somma = Somma;
+        Console.WriteLine($"Somma tra 5 e 7: {op_somma(5, 7)}");
+        Console.WriteLine($"Somma con 'EseguiOperazione: {EseguiOperazione(5, 7, op_somma)}");
+
+        Console.WriteLine("");
+
+        Operazione op_moltiplicazione = Moltiplicazione;
+        Console.WriteLine($"Moltiplicazione tra 5 e 7: {op_moltiplicazione(5, 7)}");
+        Console.WriteLine($"Moltiplicazione con 'EseguiOperazione: {EseguiOperazione(5, 7, op_moltiplicazione)}");
+    }
+}
